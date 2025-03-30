3fbb398893b7e2b3b3a62df47ced6b7a7a8b04c4
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2025-03-30 22:10:09 +0300

    acceleration and braking forces on car

--- Assets/Scripts/CustomWheelCollider.cs
+++ Assets/Scripts/CustomWheelCollider.cs
@@ -4,29 +4,68 @@ using UnityEngine;
 
 public class CustomWheelCollider : MonoBehaviour
 {
-    private Rigidbody rb;
+    [HideInInspector] public bool isBraking;
+    [HideInInspector] public float steerAngle;
+    [HideInInspector] public float motorTorque;
     
+    private Rigidbody _rigidbody;
+
+    [SerializeField] private float wheelRadius;
     [SerializeField] private float springForce;
     [SerializeField] private float dampingForce;
+    [SerializeField] private float brakingForce;
+    [SerializeField] private float lateralFriction;
     [SerializeField] private float springRestDistance;
+    
+    [SerializeField] private bool isDrivingWheel;
+    [SerializeField] private bool isSteeringWheel;
 
     private void Awake()
     {
-        rb = GetComponentInParent<Rigidbody>();
+        _rigidbody = GetComponentInParent<Rigidbody>();
     }
     
     private void FixedUpdate()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, springRestDistance * 2f))
+        var springDir = transform.up;
+        Vector3 point = transform.position;
+        var pointVelocity = _rigidbody.GetPointVelocity(transform.position);
+        Debug.DrawRay(transform.position, -springDir * springRestDistance * 2f, Color.red);
+        if (Physics.Raycast(transform.position, -springDir, out RaycastHit hit, springRestDistance * 2f))
         {
-            Debug.DrawRay(transform.position, Vector3.down * hit.distance, Color.red);
-
-            var velocity = new Vector3(0f, rb.GetPointVelocity(transform.position).y, 0f);
+            point = hit.point;
+            var velocity = Vector3.Dot(springDir, pointVelocity);
             var offset = springRestDistance - hit.distance;
-            var spring = springForce * offset * Vector3.up;
-            var damper = velocity * dampingForce; 
-            var force = spring - damper; 
-            rb.AddForceAtPosition(force, transform.position, ForceMode.Acceleration); 
+            var force = (offset * springForce) - (velocity * dampingForce);
+            _rigidbody.AddForceAtPosition(springDir * force, point, ForceMode.Acceleration); 
         }
+
+        if (isDrivingWheel)
+        {
+            var forwardForce = motorTorque / wheelRadius;
+            _rigidbody.AddForceAtPosition(transform.forward * forwardForce, point, ForceMode.Acceleration);
+        }
+        
+        var horizontalVelocity = new Vector3(pointVelocity.x, 0f, pointVelocity.z);
+        if (isBraking)
+        {
+            var brakeForce = 0.8f * Vector3.Dot(horizontalVelocity, -transform.forward);
+            _rigidbody.AddForceAtPosition(transform.forward * brakeForce, point, ForceMode.Acceleration);
+
+            if (_rigidbody.velocity.magnitude < 0.1f)
+            {
+                _rigidbody.velocity = Vector3.zero; 
+            }
+        }
+
+        if (isSteeringWheel)
+        {
+            var rotation = transform.localRotation.eulerAngles;
+            rotation.y = steerAngle;
+            transform.localRotation = Quaternion.Euler(rotation);
+        }
+        
+        var lateralCorrection = lateralFriction * Vector3.Dot(horizontalVelocity, transform.right);  
+        _rigidbody.AddForceAtPosition(transform.right * -lateralCorrection, point, ForceMode.Acceleration);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private CustomWheelCollider frontLeftWheel;
+    [SerializeField] private CustomWheelCollider frontRightWheel;
+    [SerializeField] private CustomWheelCollider backLeftWheel;
+    [SerializeField] private CustomWheelCollider backRightWheel;
+    
+    [SerializeField] private float motorTorque;
+    [SerializeField] private float brakeTorque;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        var horizontal = Input.GetAxis("Horizontal");
         
+        frontLeftWheel.steerAngle = horizontal * 35f; 
+        frontRightWheel.steerAngle = horizontal * 35f;
+
+        backLeftWheel.motorTorque = (Input.GetKey(KeyCode.W)) ? motorTorque : 0f;
+        backRightWheel.motorTorque = (Input.GetKey(KeyCode.W)) ? motorTorque : 0f;
+
+        backLeftWheel.isBraking = Input.GetKey(KeyCode.S);
+        backRightWheel.isBraking = Input.GetKey(KeyCode.S);
     }
 }
